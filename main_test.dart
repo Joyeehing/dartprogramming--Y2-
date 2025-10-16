538e635875c277fd8f0f8ce707830905c8ded8fe
Joyeehing <hingsiyun@gmail.com>
2025-10-16 20:23:56 +0100

    initial commit

---
+++ main_test.dart
@@ -0,0 +1,9 @@
+ // Write a function that returns the sum of two integers
+int sum(int a, int b) {
+  return a + b;
+}
+//write a main function to test the sum function
+void main() {
+  int result = sum(3, 5);
+  print("The sum of 3 and 5 is: $result");
+}
